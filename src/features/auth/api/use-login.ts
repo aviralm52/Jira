288b220a6e1d57a7742f5116e63c1c5bc45b0eb4
aviralm52 <aviralm52@gmail.com>
2024-05-29 09:03:37 +0530

    created workspaces and uploaded images

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,8 +1,10 @@
+import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
-import { useRouter } from "next/navigation";
+
+import { toast } from "sonner";
 
 type RequestType = InferRequestType<(typeof client.api.auth.login)["$post"]>;
 type ResponseType = InferResponseType<(typeof client.api.auth.login)["$post"]>;
@@ -14,11 +16,20 @@ export const useLogin = () => {
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.auth.login["$post"]({ json });
+
+      if (!response.ok) {
+        throw new Error("Failed to Login");
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Logged in");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+    onError: () => {
+      toast.error("Failed to Log In");
     },
   });
   return mutation;
--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -4,6 +4,8 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
 
+import { toast } from "sonner";
+
 type RequestType = InferRequestType<(typeof client.api.auth.logout)["$post"]>;
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
@@ -14,11 +16,20 @@ export const useLogout = () => {
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth.logout["$post"]();
+
+      if (!response.ok) {
+        throw new Error("Failed to Logout");
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Logged Out");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+    onError: () => {
+      toast.error("Failed to Log Out");
     },
   });
   return mutation;
